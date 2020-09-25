bb1af24eef104d6dc52b8ba46f1408153b835b1e
paola-homsi <paola.homsi24@gmail.com>
2020-09-25 14:01:09 +0200

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BoardComponent } from './board/board.component';
+import { BoardService } from './services/board.service';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BoardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [BoardService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
